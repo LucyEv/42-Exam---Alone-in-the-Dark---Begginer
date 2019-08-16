fcbaa76dbe182be930dddc2224277d329252fde4
Lucy <47707901+LucyEv@users.noreply.github.com>
2019-08-16 20:05:56 +0300

    Add files via upload

---
+++ 0-1/first_word/first_word.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   first_word.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: levstrat <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/02/25 12:41:57 by levstrat          #+#    #+#             */
+/*   Updated: 2019/02/25 13:18:59 by levstrat         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_first_word(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n')
+		i++;
+	while (str[i] != ' ' && str[i] != '\t' && str[i] != '\n' && str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+	ft_putchar('\n');
+}
+
+int	main(int ac, char **av)
+{
+	if (ac == 2)
+	{
+		ft_first_word(av[1]);
+		return (0);
+	}
+	else
+		ft_putchar('\n');
+	return (0);
+}
