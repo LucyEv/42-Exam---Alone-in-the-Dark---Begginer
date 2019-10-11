b30e75766b07e80494990d2b8dda3a0af1f8a1b9
Lucy <47707901+LucyEv@users.noreply.github.com>
2019-10-11 19:45:57 +0300

    Add files via upload

---
+++ 0-2/ft_strdup/ft_strdup.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: levstrat <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/02/27 20:18:28 by levstrat          #+#    #+#             */
+/*   Updated: 2019/02/27 20:27:24 by levstrat         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char	*ft_strdup(char *src)
+{
+	int	i;
+	int	len;
+	char *str;
+
+	len = 0;
+	while (src[len])
+		len++;
+	str = (char*)malloc(sizeof(*str) * (len + 1));
+	i = 0;
+	while (i < len)
+	{
+		str[i] = src[i];
+		i++;
+	}
+	return (str);
+}
