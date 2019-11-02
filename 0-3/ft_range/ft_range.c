e396a4e80cc29c117cddc68c9e7eebd4ec17b11c
Lucy <47707901+LucyEv@users.noreply.github.com>
2019-11-02 22:26:06 +0300

    Add files via upload

---
+++ 0-3/ft_range/ft_range.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_range.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: levstrat <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/02 15:25:29 by levstrat          #+#    #+#             */
+/*   Updated: 2019/03/02 15:39:37 by levstrat         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	*ft_range(int start, int end)
+{
+	int	*range;
+	int i;
+
+	if (start > end)
+		range = (int *)malloc(sizeof(int) * (start - end) + 1);
+	else
+		range = (int *)malloc(sizeof(int) * (end - start) + 1);
+	i = 0;
+	while (start != end)
+	{
+		range[i++] = start;
+		start += (start > end) ? -1 : 1;
+	}
+	range[i] = start;
+	return (range);
+}
