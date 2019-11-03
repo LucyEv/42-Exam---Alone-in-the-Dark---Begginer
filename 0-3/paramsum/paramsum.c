94ea537a83981553a528b1aa9bec5fe9bfa8f77a
Lucy <47707901+LucyEv@users.noreply.github.com>
2019-11-03 10:04:27 +0300

    Add files via upload

---
+++ 0-3/paramsum/paramsum.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   paramsum.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: levstrat <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/06 17:39:05 by levstrat          #+#    #+#             */
+/*   Updated: 2019/04/06 18:09:54 by levstrat         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else
+		ft_putchar(nb + '0');
+}
+
+int		main(int argc, const char **argv)
+{
+	int	nb;
+
+	argv[1] = "0";
+	nb = argc - 1;
+	if (nb >= 0)
+		ft_putnbr(nb);
+	write(1, "\n", 1);
+	return (0);
+}
