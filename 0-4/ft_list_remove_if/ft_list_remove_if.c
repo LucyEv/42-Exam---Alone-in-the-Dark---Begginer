f7355bdf07ae17600449a0edfbe8ca5f75b3de81
Lucy <47707901+LucyEv@users.noreply.github.com>
2019-11-11 17:30:08 +0300

    Add files via upload

---
+++ 0-4/ft_list_remove_if/ft_list_remove_if.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_remove_if.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: levstrat <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/02/27 23:33:20 by levstrat          #+#    #+#             */
+/*   Updated: 2019/02/28 00:21:50 by levstrat         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+#include <stdlib.h>
+
+void	ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
+{
+	t_list	*tmp;
+	t_list	*i;
+
+	while (*begin_list && !cmp((*begin_list)->data, data_ref))
+	{
+		tmp = *begin_list;
+		*begin_list = (*begin_list)->next;
+		free(tmp);
+	}
+	i = *begin_list;
+	while (i && i->next)
+	{
+		if (!cmp(i->next->data, data_ref))
+		{
+			tmp = i->next;
+			i->next = tmp->next;
+			free(tmp);
+		}
+		if (i->next)
+			i = i->next;
+	}
+}
